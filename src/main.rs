4f9b74e75c620a2f6696e50f861e9d1921fb97aa
Dominik Inführ <dominik.infuehr@gmail.com>
2015-08-15 00:37:36 -0700

    read chars instead of bytes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![feature(io)]
+#![feature(unicode)]
+
 use std::io::BufReader;
 use std::io::BufWriter;
 use std::io::Read;
@@ -17,9 +20,10 @@ fn main() {
     let mut inside = false;
 
     let mut fout = BufWriter::new(File::create(Path::new(&args[2])).unwrap());
+    let mut buffer: Vec<u8> = vec![0; 4];
 
-    for ch in fin.bytes() {
-        let ch = ch.unwrap() as char;
+    for ch in fin.chars() {
+        let ch = ch.unwrap();
 
         if inside {
             if ch == '\n' {
@@ -32,7 +36,8 @@ fn main() {
             inside = true;
         }
 
-        fout.write(&[ch as u8]).unwrap();
+        ch.encode_utf8(&mut buffer).unwrap();
+        fout.write(&buffer).unwrap();
         last = ch;
     }
 }
